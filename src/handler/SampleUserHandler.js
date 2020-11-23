bb07b4e7b9fd00505989a53252afd53a723d2fca
Dileep <abhat@wiley.com>
2020-11-23 15:18:50 +0530

    structure fix

--- src/handler/SampleUserHandler.js
+++ src/handler/SampleUserHandler.js
@@ -1,51 +1,13 @@
-const uuid = require('uuid');
-const httpStatus = require('http-status');
+const UserModel = require('../model/UserModel');
 
-const dbClient = require('../config/DbClient');
-
-module.exports.list = (event, context, callback) => {
-
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE_USER
-    };
-
-    dbClient.scan(params, (err, results) => {
-        if (err) {
-            callback(null, {
-                statusCode: error.statusCode || httpStatus.NOT_IMPLEMENTED
-            });
-        }
-        callback(null, {
-            statusCode: httpStatus.OK,
-            body: JSON.stringify(results)
-        });
-    });
+module.exports.list = async () => {
+    const results = await UserModel.list();
+    return results;
 };
 
 
-module.exports.create = (event, context, callback) => {
-    const timestamp = new Date().getTime();
 
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE_USER,
-        Item: {
-            id: uuid.v1(),
-            createdAt: timestamp,
-            updatedAt: timestamp,
-        }
-    };
-
-    // write the todo to the database
-    return dbClient.put(params, error => {
-        if (error) {
-            callback(null, {
-                statusCode: error.statusCode || httpStatus.NOT_IMPLEMENTED
-            });
-        }
-        callback(null, {
-            statusCode: httpStatus.OK,
-            body: JSON.stringify(params.Item),
-        });
-    });
-
+module.exports.create = async () => {
+    const results = await UserModel.create();
+    return results;
 };
